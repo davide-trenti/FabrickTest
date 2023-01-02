d8bbcceff55d6e7823547f7dfe108346b0f7b0d3
Davide Trenti <davide.trenti@amaris.com>
2023-01-02 15:06:56 +0100

    first commit

---
+++ src/main/java/com/fabricktest/FabricktestApplication.java
@@ -0,0 +1,13 @@
+package com.fabricktest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FabricktestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FabricktestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fabricktest/FabricktestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fabricktest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FabricktestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
