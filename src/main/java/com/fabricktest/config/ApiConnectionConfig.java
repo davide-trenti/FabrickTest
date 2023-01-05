c956d9bf018173f2529ff7ec803677bacfb3981c
Davide Trenti <davide.trenti@amaris.com>
2023-01-05 19:32:15 +0100

    updated config class

--- src/main/java/com/fabricktest/config/ApiConnectionConfig.java
+++ src/main/java/com/fabricktest/config/ApiConnectionConfig.java
@@ -3,11 +3,23 @@ package com.fabricktest.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.PropertySource;
 
+import lombok.Setter;
+
+import lombok.Getter;
+
+import lombok.NoArgsConstructor;
+
+import lombok.AllArgsConstructor;
+
 /*
  * 
  * @author Davide Trenti
  */
 
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 @PropertySource(value={"classpath:application.properties"})
 public class ApiConnectionConfig {
 
@@ -23,67 +35,5 @@ public class ApiConnectionConfig {
 	String accountTransactionList;
 	@Value("${uri.accountMoneyTransfer}")
 	String accountMoneyTransfer;
-	
-	public ApiConnectionConfig() {
-		super();
-	}
-	
-	public ApiConnectionConfig(String sourceAddress, String keyValue, String authSchemaValue,
-			String accountBalance, String accountTransactionList, String accountMoneyTransfer) {
-		super();
-		this.sourceAddress = sourceAddress;
-		this.keyValue = keyValue;
-		this.authSchemaValue = authSchemaValue;
-		this.accountBalance = accountBalance;
-		this.accountTransactionList = accountTransactionList;
-		this.accountMoneyTransfer = accountMoneyTransfer;
-	}
-	
-	public String getSourceAddress() {
-		return sourceAddress;
-	}
-	
-	public void setSourceAddress(String sourceAddress) {
-		this.sourceAddress = sourceAddress;
-	}
-	
-	public String getKeyValue() {
-		return keyValue;
-	}
-	
-	public void setKeyValue(String keyValue) {
-		this.keyValue = keyValue;
-	}
-	
-	public String getAuthSchemaValue() {
-		return authSchemaValue;
-	}
-	
-	public void setAuthSchemaValue(String authSchemaValue) {
-		this.authSchemaValue = authSchemaValue;
-	}
-	
-	public String getAccountBalance() {
-		return accountBalance;
-	}
-	
-	public void setAccountBalance(String accountBalance) {
-		this.accountBalance = accountBalance;
-	}
-	
-	public String getAccountTransactionList() {
-		return accountTransactionList;
-	}
-	
-	public void setAccountTransactionList(String accountTransactionList) {
-		this.accountTransactionList = accountTransactionList;
-	}
-	
-	public String getAccountMoneyTransfer() {
-		return accountMoneyTransfer;
-	}
-	
-	public void setAccountMoneyTransfer(String accountMoneyTransfer) {
-		this.accountMoneyTransfer = accountMoneyTransfer;
-	}
+
 }
